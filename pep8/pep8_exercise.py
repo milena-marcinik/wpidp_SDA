63b03e0fe7064a14db18184abbee1bec10646145
Milena <milena.marcinik@gmail.com>
2020-11-14 11:55:05 +0100

    Use of a pylint in exercise

--- pep8/pep8_exercise.py
+++ pep8/pep8_exercise.py
@@ -1,4 +1,15 @@
+"""
+This module contains pep8 samples.
+"""
+
+
 def count_message_letters(message):
+    """
+    Counts letters in the given string.
+
+    :param message: string containing message to count
+    :return: number of letters
+    """
     counter = 0
     for _ in message:
         counter += 1
@@ -9,16 +20,27 @@ STRING_CONSTANT = 'Ala ma kota'
 
 
 def modify_message_to_list(message):
+    """
+
+    :param message:
+    :return:
+    """
     return [letter + '-' + message for letter in message]
 
 
-class SampleClass:
+class SampleClass:  # pylint: disable=too-few-public-methods
+    """This class represents pep8 sample."""
     SOME_CONST = 0
 
     def __init__(self):
         self.list_of_items = []
 
     def sample_method(self, item):
+        """
+
+        :param item:
+        :return:
+        """
         return self.list_of_items.append(item)
 
 
