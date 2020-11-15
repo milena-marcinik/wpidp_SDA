389be6b0f684aa1a002d2cb5c249ecebb7cd7811
Milena <milena.marcinik@gmail.com>
2020-11-15 09:02:19 +0100

    Add new directories

---
+++ design_patterns/creational/singleton.py
@@ -0,0 +1,19 @@
+class Singleton:
+    _INSTANCE = None
+
+    MAX_USER_AMOUNT = 100
+
+    def __init__(self):
+        raise NotImplementedError('Creation of singleton instance with constructor is forbidden')
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._INSTANCE:
+            cls._INSTANCE = cls.__new__(cls)
+        return cls._INSTANCE
+
+
+if __name__ == '__main__':
+    instance_1 = Singleton.get_instance()
+    instance_2 = Singleton.get_instance()
+    assert instance_1 is instance_2
