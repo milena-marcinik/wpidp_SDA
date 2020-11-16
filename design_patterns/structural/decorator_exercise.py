70f97b33b73efa8b0e4e7dc8eef362c13b094ce9
Milena <milena.marcinik@gmail.com>
2020-11-16 17:58:51 +0100

    Add the correct solution to decorator exercise

--- design_patterns/structural/decorator_exercise.py
+++ design_patterns/structural/decorator_exercise.py
@@ -12,3 +12,98 @@ zwiększać cenę pizzy.
 Przykładowo mamy do dyspozycji następujące składniki: Mozzarella, Ham, Mushrooms, Salami. KAŻDY SKLADNIK MA SWOJĄ CENĘ!
 
 """
+from abc import ABC, abstractmethod
+
+
+class Pizza(ABC):
+
+    @abstractmethod
+    def get_cost(self):
+        pass
+
+    @abstractmethod
+    def get_description(self):
+        pass
+
+
+class PlainPizza(Pizza):
+
+    def __init__(self, cost):
+        self._cost = cost
+        self._description = "base of pizza"
+
+    def get_cost(self):
+        return self._cost
+
+    def get_description(self):
+        return self._description
+
+
+class PizzaDecorator(Pizza):
+
+    def __init__(self, plain: Pizza):
+        self._wrappee = plain
+
+    def get_cost(self):
+        return self._wrappee.get_cost()
+
+    def get_description(self):
+        return self._wrappee.get_description()
+
+
+class Mozzarella(PizzaDecorator):
+    _COST = 2
+    _DESCRIPTION = "mozzarella"
+
+    def get_cost(self):
+        return self._COST + super().get_cost()
+
+    def get_description(self):
+        return f"{self._DESCRIPTION}, {super().get_description()}"
+
+
+class Ham(PizzaDecorator):
+    _COST = 4
+    _DESCRIPTION = "ham"
+
+    def get_cost(self):
+        return self._COST + super().get_cost()
+
+    def get_description(self):
+        return f"{self._DESCRIPTION}, {super().get_description()}"
+
+
+class Mushrooms(PizzaDecorator):
+    _COST = 3
+    _DESCRIPTION = "mushrooms"
+
+    def get_cost(self):
+        return self._COST + super().get_cost()
+
+    def get_description(self):
+        return f"{self._DESCRIPTION}, {super().get_description()}"
+
+
+class Salami(PizzaDecorator):
+    _COST = 5
+    _DESCRIPTION = "salami"
+
+    def get_cost(self):
+        return self._COST + super().get_cost()
+
+    def get_description(self):
+        return f"{self._DESCRIPTION}, {super().get_description()}"
+
+
+if __name__ == '__main__':
+    plain_pizza = PlainPizza(10)
+
+    # ham pizza
+    ham_pizza = Ham(plain_pizza)
+    print(ham_pizza.get_cost())
+    print(ham_pizza.get_description())
+
+    # ham and mushroom pizza
+    ham_mushroom_pizza = Mushrooms(ham_pizza)
+    print(ham_mushroom_pizza.get_cost())
+    print(ham_mushroom_pizza.get_description())
